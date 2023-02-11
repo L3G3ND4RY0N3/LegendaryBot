7ea85b92a21ac86cccd112fab95bb7abcb914427
L3G3ND <carl.max@hotmail.de>
2023-02-11 16:42:17 +0100

    Erfolgreich das erste Cog mit Ping und Hi Command hinzugefügt

--- modules/Ping/cog.py
+++ modules/Ping/cog.py
@@ -1,19 +1,21 @@
 import discord
 from discord.ext import commands
 
-class Ping(commands.Cog, name = "Ping"):
-    def __innit__(self, bot: commands.Bot):
+class Ping(commands.Cog, name="Ping"):
+    def __innit__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
+    @commands.Cog.listener() #ansatt bot.event!
     async def on_ready(self):
         print("Ping.py is ready!")    
 
     @commands.command()
-    async def ping(self, ctx: commands.Context):
-        bot_latency = round(self.bot.latency * 1000)
+    async def ping(self, ctx):
+        await ctx.send(f"Ping {ctx.author.mention}!")
 
-        await ctx.send(f"Football.")
+    @commands.command()
+    async def hi(self, ctx):
+        await ctx.send(f"Hello there {ctx.author.mention}")
 
-async def setup(bot: commands.Bot):
+async def setup(bot):
     await bot.add_cog(Ping(bot))
