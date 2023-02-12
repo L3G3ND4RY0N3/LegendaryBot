d490a72aed36b5be73e8446d4649cc126d8462ae
L3G3ND <carl.max@hotmail.de>
2023-02-12 18:18:21 +0100

    Autoroles Slash Command :D

--- modules/Autoroles/cog.py
+++ modules/Autoroles/cog.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 import json
 
 class Autoroles(commands.Cog, name="Autoroles"):
@@ -36,6 +37,22 @@ class Autoroles(commands.Cog, name="Autoroles"):
         
         await ctx.send(embed=conf_embed)
 
+    @app_commands.command(name="add_autorole", description="Adds a role that is automatically assigned to a new member!")
+    @app_commands.describe(role = "Select the role")
+    async def add_join_role(self, interaction: discord.Interaction, role: discord.Role):
+        with open("modules/Autoroles/json/autoroles.json", "r") as f:
+            auto_role = json.load(f)
+
+        auto_role[str(role.guild.id)] = str(role.name)
+
+        with open("modules/Autoroles/json/autoroles.json", "w") as f:
+            json.dump(auto_role, f, indent=4)
+
+        conf_embed = discord.Embed(color=discord.Color.green())
+        conf_embed.add_field(name="Success!", value=f"The automatic role for this guild/server has been set to {role.mention}.")
+        conf_embed.set_footer(text=f"Action taken by {interaction.user}.")
+        
+        await interaction.response.send_message(embed=conf_embed)
 
 async def setup(bot):
     await bot.add_cog(Autoroles(bot))
