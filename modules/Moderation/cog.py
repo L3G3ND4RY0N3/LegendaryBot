c3a7f85cb7ed35307b2dede83359d44fa75d788b
L3G3ND <carl.max@hotmail.de>
2023-02-19 15:19:31 +0100

    Json Support für alle Slap Commands etc. sowie Ban und Delete Message Commands

--- modules/Moderation/cog.py
+++ modules/Moderation/cog.py
@@ -10,6 +10,46 @@ class Moderation(commands.Cog, name="Moderation"):
     async def on_ready(self):
         print("Moderation.py is ready!")    
 
+
+
+
+    #Delete Messages Command
+    @app_commands.command(name="delete_messages", description="Delete some messages in a channel or from a member!")
+    @app_commands.describe(number="The number of messages you want to delete?")
+    @app_commands.describe(member="Whose messages you want to delete")
+    @app_commands.describe(reason="Why is the member being kicked?")
+    @app_commands.checks.has_permissions(manage_messages=True)
+    async def delete(self, interaction: discord.Interaction, number: int, member: discord.Member=None, reason: str=None):
+        try:
+            delete_counter = 0
+            async for message in interaction.channel.history():
+                if message.author == member or member == None:
+                    await message.delete()
+                    delete_counter += 1
+                if delete_counter == number:
+                    conf_embed = discord.Embed(title="Success!", color=discord.Color.red())
+                    conf_embed.add_field(name="Messages deleted!", value=f"{number} messages will be removed!")
+                    if reason is not None:
+                        conf_embed.add_field(name="Reason", value=f"{reason}", inline=False) #if reason is not specified, dont include in embed
+                    conf_embed.set_footer(text=f"Action taken by {interaction.user}.")
+
+                    await interaction.response.send_message(embed=conf_embed)
+                    return
+                    
+
+        except discord.Forbidden:
+            conf_embed = discord.Embed(color=discord.Color.yellow())
+            conf_embed.add_field(name="Failure!", value=f"I am missing the permissions to delete messages ('manage messages').")
+            conf_embed.set_footer(text=f"Action attempted by {interaction.user}.")
+            await interaction.response.send_message(embed=conf_embed, ephemeral=True)
+            return
+
+
+    ####################################################################################################################################
+    ##################################################### Kick Command #################################################################
+    ####################################################################################################################################
+
+    #Kick Command
     @app_commands.command(name="kick", description="Kick a member!")
     @app_commands.describe(member="Who do you want to kick?")
     @app_commands.describe(reason="Why is the member being kicked?")
@@ -27,16 +67,62 @@ class Moderation(commands.Cog, name="Moderation"):
         conf_embed = discord.Embed(title="Success!", color=discord.Color.red())
         conf_embed.add_field(name="Member kicked!", value=f"{member.mention} has been kicked from the server!")
         if reason is not None:
-            conf_embed.add_field(name="Reason", value=f"{reason}", inline=False)
+            conf_embed.add_field(name="Reason", value=f"{reason}", inline=False) #if reason is not specified, dont include in embed
         conf_embed.set_footer(text=f"Action taken by {interaction.user}.")
 
         await interaction.response.send_message(embed=conf_embed)
 
+    ####################################################################################################################################
+    ##################################################### Ban Command ##################################################################
+    ####################################################################################################################################
+
+    
+    @app_commands.command(name="ban", description="Ban a member!")
+    @app_commands.describe(member="Who do you want to ban?")
+    @app_commands.describe(reason="Why is the member being banned?")
+    @app_commands.checks.has_permissions(ban_members=True)
+    async def ban(self, interaction: discord.Interaction, member: discord.Member, reason: str=None):
+        try:
+            await member.kick()
+        except discord.Forbidden:
+            conf_embed = discord.Embed(color=discord.Color.yellow())
+            conf_embed.add_field(name="Failure!", value=f"I am missing the permissions to ban {member.mention}.")
+            conf_embed.set_footer(text=f"Action attempted by {interaction.user}.")
+            await interaction.response.send_message(embed=conf_embed, ephemeral=True)
+            return
+
+        conf_embed = discord.Embed(title="Success!", color=discord.Color.red())
+        conf_embed.add_field(name="Member banned!", value=f"{member.mention} has been banned from the server!")
+        if reason is not None:
+            conf_embed.add_field(name="Reason", value=f"{reason}", inline=False) #if reason is not specified, dont include in embed
+        conf_embed.set_footer(text=f"Action taken by {interaction.user}.")
+
+        await interaction.response.send_message(embed=conf_embed)
+
+
+    ####################################################################################################################################
+    ##################################################Error Handeling!##################################################################
+    ####################################################################################################################################
+
+
+
+    #On command error
     @commands.Cog.listener()
     async def on_application_command_error(self, interaction: discord.Interaction, error):
         await interaction.response.send_message(f"Following error has occured: ```{error}```")
         raise error
 
+
+    #Delete error, when user lacks permissions
+    @delete.error
+    async def delete_error(self,interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingPermissions):
+            conf_embed = discord.Embed(color=discord.Color.red())
+            conf_embed.add_field(name="Failure!", value=f"{interaction.user}, you do not have the permissions to delete messages!")
+            conf_embed.set_footer(text=f"Action attempted by {interaction.user}.")
+            await interaction.response.send_message(embed=conf_embed, ephemeral=True)
+
+    #Kick error, when user lacks permissions
     @kick.error
     async def kick_error(self,interaction: discord.Interaction, error: app_commands.AppCommandError):
         if isinstance(error, app_commands.MissingPermissions):
@@ -45,6 +131,15 @@ class Moderation(commands.Cog, name="Moderation"):
             conf_embed.set_footer(text=f"Action attempted by {interaction.user}.")
             await interaction.response.send_message(embed=conf_embed, ephemeral=True)
 
+
+    #Ban error, when user lacks permissions
+    @ban.error
+    async def ban_error(self,interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingPermissions):
+            conf_embed = discord.Embed(color=discord.Color.red())
+            conf_embed.add_field(name="Failure!", value=f"{interaction.user}, you do not have the permissions to ban a member!")
+            conf_embed.set_footer(text=f"Action attempted by {interaction.user}.")
+            await interaction.response.send_message(embed=conf_embed, ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
--- modules/Slap/cog.py
+++ modules/Slap/cog.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import json
 
 class Slap(commands.Cog, name="Slaps"):
     def __innit__(self, bot):
@@ -14,8 +15,21 @@ class Slap(commands.Cog, name="Slaps"):
     @app_commands.command(name="slapjura", description="Gib Jura eine Schelle, weil er das verdient hat!")
     async def slapjura(self, interaction: discord.Interaction):
         jura = 768786872184078357
+
+        with open("modules/Slap/json/jura.json", "r+") as f:
+            jura_data = json.load(f)
+            if str(interaction.guild.id) not in jura_data:
+                jura_data[str(interaction.guild.id)] = 0
+
+            jura_data[str(interaction.guild.id)] = jura_data[str(interaction.guild.id)] + 1
+            amount = jura_data[str(interaction.guild.id)]
+
+            f.seek(0)
+       
+            json.dump(jura_data, f, indent=4)
+
         conf_embed = discord.Embed(color=discord.Color.blue())
-        conf_embed.add_field(name="Slap!", value=f"<@{jura}> kriegt ne heftige Schelle!")
+        conf_embed.add_field(name="Slap!", value=f"<@{jura}> kriegt ne heftige Schelle! Das ist bereits die {amount}. Schelle, die er sich verdient hat!")
         conf_embed.set_footer(text=f"Verteilt durch {interaction.user}.")
         await interaction.response.send_message(embed=conf_embed)
 
@@ -33,9 +47,50 @@ class Slap(commands.Cog, name="Slaps"):
     @app_commands.command(name="hug", description="Give someone a hug!")
     @app_commands.describe(user = "Who do you want to hug?")
     async def hug(self, interaction: discord.Interaction, user: discord.Member):
+
+        with open("modules/Slap/json/hugs.json", "r+") as f:
+            hug_data = json.load(f)
+
+            if str(interaction.guild.id) not in hug_data:
+                hug_data[str(interaction.guild.id)] = {}
+
+            if str(user.id) not in hug_data[str(interaction.guild.id)]:
+                hug_data[str(interaction.guild.id)].update({str(user.id): 0})
+            hug_data[str(interaction.guild.id)][str(user.id)] = hug_data[str(interaction.guild.id)][str(user.id)] + 1
+            amount = hug_data[str(interaction.guild.id)][str(user.id)]
+        
+            f.seek(0)
+       
+            json.dump(hug_data, f, indent=4)
+
         conf_embed = discord.Embed(color=discord.Color.green())
-        conf_embed.add_field(name="Hugged!", value=f"{interaction.user.mention} gives {user.mention} a hug <3")
+        conf_embed.add_field(name="Hugged!", value=f"{interaction.user.mention} gives {user.mention} a hug <3 This is the {amount}. hug given to {user.name}!")
         conf_embed.set_footer(text=f"Verteilt durch {interaction.user}.")
+        await interaction.response.send_message(embed=conf_embed)
+
+    #Pat a Person with Mention!
+    @app_commands.command(name="pat", description="Pat someone!")
+    @app_commands.describe(user = "Who do you want to pat?")
+    async def pat(self, interaction: discord.Interaction, user: discord.Member):
+
+        with open("modules/Slap/json/pats.json", "r+") as f:
+            pat_data = json.load(f)
+
+            if str(interaction.guild.id) not in pat_data:
+                pat_data[str(interaction.guild.id)] = {}
+
+            if str(user.id) not in pat_data[str(interaction.guild.id)]:
+                pat_data[str(interaction.guild.id)].update({str(user.id): 0})
+            pat_data[str(interaction.guild.id)][str(user.id)] = pat_data[str(interaction.guild.id)][str(user.id)] + 1
+            amount = pat_data[str(interaction.guild.id)][str(user.id)]
+        
+            f.seek(0)
+       
+            json.dump(pat_data, f, indent=4)
+
+        conf_embed = discord.Embed(color=discord.Color.green())
+        conf_embed.add_field(name="Pat!", value=f"{interaction.user.mention} pats {user.mention} because they like them! <3 This is the {amount}. pat given to {user.name}!")
+        conf_embed.set_footer(text=f"Patted by {interaction.user}.")
         await interaction.response.send_message(embed=conf_embed)
 
     #Slap a Person with Mention!
@@ -46,9 +101,25 @@ class Slap(commands.Cog, name="Slaps"):
             conf_embed = discord.Embed(color=discord.Color.red())
             conf_embed.add_field(name="Failure!", value=f"{interaction.user.mention}, you lack the permission to slap L3G3ND! Nice try though.")
             conf_embed.set_footer(text=f"Attempted by {interaction.user}.")
-            await interaction.response.send_message(embed=conf_embed)
+            await interaction.response.send_message(embed=conf_embed, ephemeral=True)
+
+        with open("modules/Slap/json/slaps.json", "r+") as f:
+            slap_data = json.load(f)
+
+            if str(interaction.guild.id) not in slap_data:
+                slap_data[str(interaction.guild.id)] = {}
+
+            if str(user.id) not in slap_data[str(interaction.guild.id)]:
+                slap_data[str(interaction.guild.id)].update({str(user.id): 0})
+            slap_data[str(interaction.guild.id)][str(user.id)] = slap_data[str(interaction.guild.id)][str(user.id)] + 1
+            amount = slap_data[str(interaction.guild.id)][str(user.id)]
+        
+            f.seek(0)
+       
+            json.dump(slap_data, f, indent=4)
+        
         conf_embed = discord.Embed(color=discord.Color.blue())
-        conf_embed.add_field(name="Slap!", value=f"{user.mention} was slapped by {interaction.user.mention}!.")
+        conf_embed.add_field(name="Slap!", value=f"{user.mention} was slapped by {interaction.user.mention}! This is the {amount}. slap!")
         conf_embed.set_footer(text=f"Slap issued by {interaction.user}.")
         await interaction.response.send_message(embed=conf_embed)
 
