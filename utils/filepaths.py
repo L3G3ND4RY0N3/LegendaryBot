611fd7b8631bbdfc93a00d150fe166a655c41d25
L3G3ND <carl.max@hotmail.de>
2023-12-15 20:46:43 +0100

    Add filepath module Rekated to issue #14

---
+++ utils/filepaths.py
@@ -0,0 +1,9 @@
+
+##################### temp vc paths
+temp_vc_fp = "modules/TemporaryVoiceChannel/json/"
+temp_vc_json = "modules/TemporaryVoiceChannel/json/tempchannels.json"
+temp_creation_vc_json = "modules/TemporaryVoiceChannel/json/tempcreationvc.json"
+
+
+##################### auto role paths
+join_roles_json = "modules/Autoroles/json/autoroles.json"
