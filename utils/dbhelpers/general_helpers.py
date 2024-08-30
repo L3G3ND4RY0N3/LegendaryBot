a071769c9eaee7d5f053148a44532138d0031ac3
Carl Walde <c.walde@steueragenten.de>
2024-08-30 14:52:01 +0200

    valid attribute function

---
+++ utils/dbhelpers/general_helpers.py
@@ -0,0 +1,8 @@
+from sqlalchemy.inspection import inspect
+
+
+def get_valid_attributes(model):
+    """Automatically determine valid attribute names for sorting based on the model's columns."""
+    mapper = inspect(model)
+    valid_attributes = set(column.key for column in mapper.columns)
+    return valid_attributes
