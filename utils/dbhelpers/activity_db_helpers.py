384cf9feffe8c14e4f71207cf252c876010fd725
Carl Walde <c.walde@steueragenten.de>
2024-08-30 15:30:49 +0200

    added logging and exception handling to guild leaderboard handler

--- utils/dbhelpers/activity_db_helpers.py
+++ utils/dbhelpers/activity_db_helpers.py
@@ -5,6 +5,10 @@ from .dbservice import DatabaseService
 from.general_helpers import get_valid_attributes
 from sqlalchemy import func, desc
 from sqlalchemy.orm import Session
+from utils import settings
+
+
+logger=settings.logging.getLogger("discord")
 
 
 db_service = DatabaseService(SessionLocal)
@@ -24,16 +28,15 @@ def handle_activity_update(dcuser: discord.User | discord.Member, minutes: int =
         if activity:
             activity.update_member_activity(minutes, messages, xp)
 
-# TODO: Error handling when user is type discord.User?
-def handle_stats_command(dcuser: discord.Member) -> dict:
-    with db_service.session_scope() as session:
-        _, _, _, activity = get_or_create_for_activity(dcuser, session)
-        return activity.to_dict()
 
-
+#region LEADERBOARD
 def handle_guild_leaderboard(dcuser: discord.Member, sort_by: str = 'xp', limit: int = 10):
-    if sort_by not in get_valid_attributes(Activity):
-        raise ValueError(f"Invalid sorting attribute: {sort_by}")
+    try:
+        if sort_by not in get_valid_attributes(Activity):
+            raise ValueError(f"Invalid sorting attribute: {sort_by}")
+    except ValueError as e:
+        logger.exception(f"{e}")
+        return
     guild_id = dcuser.guild.id
     with db_service.session_scope() as session:
         activity_query = (session.query(Activity)
@@ -44,13 +47,24 @@ def handle_guild_leaderboard(dcuser: discord.Member, sort_by: str = 'xp', limit:
                           )
         activities = activity_query.all()
 
+        activity_dict_list = list[dict]
+        for activity in activities:
+            activity_dict_list.append(activity.to_dict())
 
+#endregion
+
+#region DEBUG
+# TODO: Error handling when user is type discord.User?
+def handle_stats_command(dcuser: discord.Member) -> dict:
+    with db_service.session_scope() as session:
+        _, _, _, activity = get_or_create_for_activity(dcuser, session)
+        return activity.to_dict()
 
 def display_test(dcuser: discord.User | discord.Member) -> str:
     with db_service.session_scope() as session:
         user, guild, member, activity = get_or_create_for_activity(dcuser, session)
         return f"{user.name} with id: {member.id} in guild: {member.guild.guild_dc_id} = {guild.name} and msg_count: {activity.message_count}"
-
+#endregion
 
 #region GENEREAL USE
 # TODO: consider Named Tuple returns
