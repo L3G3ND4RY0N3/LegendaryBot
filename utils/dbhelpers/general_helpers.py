9fa9cf9a7b42af85041b123538b6ca5397bf9b06
Carl Walde <c.walde@steueragenten.de>
2024-08-31 17:37:00 +0200

    added str cast to valid attr helper

--- utils/dbhelpers/general_helpers.py
+++ utils/dbhelpers/general_helpers.py
@@ -4,5 +4,5 @@ from sqlalchemy.inspection import inspect
 def get_valid_attributes(model):
     """Automatically determine valid attribute names for sorting based on the model's columns."""
     mapper = inspect(model)
-    valid_attributes = set(column.key for column in mapper.columns)
+    valid_attributes = set(str(column.key) for column in mapper.columns)
     return valid_attributes
