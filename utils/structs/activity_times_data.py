ec27be8d15c7eef5054d66cf91da26e43c1dca6e
L3G3ND <carl.max@hotmail.de>
2024-09-05 19:02:55 +0200

    removed unnecessary channel id from SessionData for activity sessions

--- utils/structs/activity_times_data.py
+++ utils/structs/activity_times_data.py
@@ -7,16 +7,13 @@ import datetime as dt
 class SessionData():
     """A dataclass that stores the ids for the guild and channel and the relevant time of a members voice state"""
     guild_id: int = None
-    voice_channel_id: int = None
     start_time: dt.datetime = None
 
 
-    def update_session_data(self, guild_id: int | None = None, channel_id: int | None = None, time: dt.datetime | None = None) -> None:
+    def update_session_data(self, guild_id: int | None = None,  time: dt.datetime | None = None) -> None:
         """Updates the session data, if guild, voice channel changes or time gets updated"""
         if guild_id:
             self.guild_id = guild_id
-        if channel_id:
-            self.voice_channel_id = channel_id
         if time:
             self.start_time = time
 
@@ -28,13 +25,13 @@ class SessionManager():
         self.sessions: dict[int, SessionData] = {}
 
 
-    def add_session(self, member_id: int, guild_id: int, channel_id: int) -> None:
+    def add_session(self, member_id: int, guild_id: int) -> None:
         """Adds the session of a member, if the member has already a session ongoing, skip"""
         time = dt.datetime.now()
         # TODO: find a satisfying fix to allow a member to be in multiple channels at once (uuid?)
         if member_id in self.sessions.keys():
             return
-        self.sessions[member_id] = SessionData(guild_id=guild_id, voice_channel_id=channel_id, start_time=time)
+        self.sessions[member_id] = SessionData(guild_id=guild_id, start_time=time)
 
 
     def get_session_for_member(self, member_id: int) -> SessionData | None:
@@ -42,17 +39,17 @@ class SessionManager():
         return self.sessions.get(member_id, None)
 
 
-    def remove_session(self, member_id) -> SessionData | None:
+    def remove_session(self, member_id: int) -> SessionData | None:
         return self.sessions.pop(member_id, None)
 
 
-    def update_session(self, member_id: int, guild_id: int | None = None, channel_id: int| None = None, time: dt.datetime | None = None) -> None:
-        """Updates the session, if a member joins, leaves or switches voice channels"""
+    def update_session(self, member_id: int, guild_id: int | None = None, time: dt.datetime | None = None) -> None:
+        """Updates the session, if a switches guilds or the start time changes after points are awarded"""
         if member_id not in self.sessions.keys():
             # TODO: Check if this could lead to issues with race conditions (e.g. member gets updated at the same time he leaves the channel)
             # self.add_session(member_id, guild_id, channel_id)
             return
-        self.sessions[member_id].update_session_data(guild_id=guild_id, channel_id=channel_id, time=time)
+        self.sessions[member_id].update_session_data(guild_id=guild_id, time=time)
 
 
     def get_guild_member(self, client: discord.Client, member_id: int) -> discord.Member | None:
