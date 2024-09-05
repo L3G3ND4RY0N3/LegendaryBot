fc64c533a8d4990905ef79a53260b6ccf1191168
L3G3ND <carl.max@hotmail.de>
2024-09-05 19:02:10 +0200

    added default for dict keys of activity

--- utils/embeds/activity_embeds.py
+++ utils/embeds/activity_embeds.py
@@ -9,9 +9,9 @@ def activity_stats_embed(stats: dict, member: discord.Member) -> discord.Embed:
     emb = discord.Embed(color=discord.Color.blurple(),
                         title=f"Activity stats for {member.name}"
                         )
-    emb.add_field(name="Sent Messages", value=f'Messages sent: {stats.get("message_count")}', inline=False)
-    emb.add_field(name="Time in Voice", value=f'Minutes in Voice: {stats.get("minutes_in_voice")}', inline=False)
-    emb.add_field(name="XP", value=f'Experience: {stats.get("xp")}', inline=False)
+    emb.add_field(name="Sent Messages", value=f'Messages sent: {stats.get("message_count", 0)}', inline=False)
+    emb.add_field(name="Time in Voice", value=f'Minutes in Voice: {stats.get("minutes_in_voice", 0)}', inline=False)
+    emb.add_field(name="XP", value=f'Experience: {stats.get("xp", 0)}', inline=False)
     emb.set_footer(text=f"Requested by {member.global_name}")
     return emb
 
