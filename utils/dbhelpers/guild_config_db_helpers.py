2aec288d876f4753e7223162f64c314f4f6ba85a
Carl <c.walde@steueragenten.de>
2024-09-15 09:57:26 +0200

    added db handler for guildconfig updates

---
+++ utils/dbhelpers/guild_config_db_helpers.py
@@ -0,0 +1,36 @@
+from typing import Any
+import discord
+from dbmodels.base import SessionLocal
+from dbmodels import Guild, GuildConfig
+from .dbservice import DatabaseService
+from sqlalchemy.orm import Session
+from utils import settings
+
+
+logger=settings.logging.getLogger("discord")
+
+db_service = DatabaseService(SessionLocal)
+
+
+def handle_guild_config_update(dcguild: discord.Guild,
+                               error_id: int | None = None,
+                               welcome_id: int | None = None,
+                               boost_id: int | None = None,
+                               log_id: int | None = None,
+                               activity: bool | None = None,) -> None:
+    with db_service.session_scope() as session:
+        guild_config = get_or_create_for_guild_config(dcguild, session=session)
+
+        if activity:
+            guild_config.update_activity(activity=activity)
+        
+        guild_config.update_channels(error_id=error_id, welcome_id=welcome_id, boost_id=boost_id, log_id=log_id)
+
+
+def get_or_create_for_guild_config(dcguild: discord.Guild, session: Session) -> GuildConfig:
+    """creates all relevant models for the activity updates"""
+    guild = db_service.get_or_create(Guild, guild_dc_id=dcguild.id, name=dcguild.name, session=session)
+    session.flush()
+    guild_config = db_service.get_or_create(GuildConfig, session=session)
+    session.commit()
+    return guild_config
