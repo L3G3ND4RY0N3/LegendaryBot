0d480572cfd2dc324c0b573abbe80cdb680474b4
Carl <c.walde@steueragenten.de>
2024-09-15 10:46:03 +0200

    added query for all activity guilds

--- utils/dbhelpers/guild_config_db_helpers.py
+++ utils/dbhelpers/guild_config_db_helpers.py
@@ -17,14 +17,24 @@ def handle_guild_config_update(dcguild: discord.Guild,
                                welcome_id: int | None = None,
                                boost_id: int | None = None,
                                log_id: int | None = None,
-                               activity: bool | None = None,) -> None:
+                               activity: bool | None = None) -> None:
     with db_service.session_scope() as session:
         guild_config = get_or_create_for_guild_config(dcguild, session=session)
 
-        if activity:
+        if activity is not None:
             guild_config.update_activity(activity=activity)
         
         guild_config.update_channels(error_id=error_id, welcome_id=welcome_id, boost_id=boost_id, log_id=log_id)
+
+
+def get_all_activity_guilds() -> set[int]:
+    with db_service.session_scope() as session:
+        guild_query = (session.query(Guild.guild_dc_id)
+                    .join(GuildConfig, full=True)
+                    .filter_by(GuildConfig.activity_status == True)
+                    )
+        guild_ids = set(guild_query.all())
+        return guild_ids
 
 
 def get_or_create_for_guild_config(dcguild: discord.Guild, session: Session) -> GuildConfig:
