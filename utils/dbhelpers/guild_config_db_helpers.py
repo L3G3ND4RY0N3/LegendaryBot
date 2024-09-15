c0f92e573fd300ad263ce8eec6f23462045f6252
Carl <c.walde@steueragenten.de>
2024-09-15 20:32:22 +0200

    added sets for guild configs

--- utils/dbhelpers/guild_config_db_helpers.py
+++ utils/dbhelpers/guild_config_db_helpers.py
@@ -11,6 +11,9 @@ logger=settings.logging.getLogger("discord")
 
 db_service = DatabaseService(SessionLocal)
 
+ACTIVITY_GUILDS = set()
+GUILDS_WITH_CONFIGS = set()
+
 
 def handle_guild_config_update(dcguild: discord.Guild,
                                error_id: int | None = None,
@@ -21,8 +24,13 @@ def handle_guild_config_update(dcguild: discord.Guild,
     with db_service.session_scope() as session:
         guild_config = get_or_create_for_guild_config(dcguild, session=session)
 
+        GUILDS_WITH_CONFIGS.add(dcguild.id)
         if activity is not None:
             guild_config.update_activity(activity=activity)
+            if activity:
+                ACTIVITY_GUILDS.add(dcguild.id)
+            else:
+                ACTIVITY_GUILDS.remove(dcguild.id)
         
         guild_config.update_channels(error_id=error_id, welcome_id=welcome_id, boost_id=boost_id, log_id=log_id)
 
@@ -31,7 +39,17 @@ def get_all_activity_guilds() -> set[int]:
     with db_service.session_scope() as session:
         guild_query = (session.query(Guild.guild_dc_id)
                     .join(GuildConfig, full=True)
-                    .filter_by(GuildConfig.activity_status == True)
+                    .filter(GuildConfig.activity_status == True)
+                    )
+        guild_ids = set(guild_query.all())
+        return guild_ids
+    
+
+def get_all_guilds_with_configs() -> set[int]:
+    with db_service.session_scope() as session:
+        guild_query = (session.query(Guild.guild_dc_id)
+                    .join(GuildConfig, full=True)
+                    .filter(GuildConfig.guild_id == Guild.id)
                     )
         guild_ids = set(guild_query.all())
         return guild_ids
