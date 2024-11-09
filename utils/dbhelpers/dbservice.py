203651503a5c2dcdcf38d6e488e9b9958d555a16
L3G3ND <carl.max@hotmail.de>
2024-11-09 15:38:17 +0100

    updated dbservice to only query unique identifiers and update all fields when changed

--- utils/dbhelpers/dbservice.py
+++ utils/dbhelpers/dbservice.py
@@ -1,5 +1,10 @@
+from typing import Type, TypeVar
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import Session
 from contextlib import contextmanager
+
+_T = TypeVar('T')
 
 class DatabaseService:
     def __init__(self, session_factory):
@@ -18,35 +23,49 @@ class DatabaseService:
         finally:
             session.close()
 
-    def get_or_create(self, model, session: Session | None = None, **kwargs):
+    def get_or_create(self, model: Type[_T], session: Session | None = None, **kwargs) -> _T:
         """Generic method to get or create a model instance."""
-        if not session:
-            with self.session_scope() as session:
-                instance = session.query(model).filter_by(**kwargs).first()
+        # Get the mapper to inspect the model's attributes and constraints
+        mapper = inspect(model)
+        
+        # Identify unique columns in the model (those with unique=True or primary_key)
+        unique_columns = []
+        for column in mapper.columns:
+            if column.unique or column.primary_key:
+                unique_columns.append(column.name)
+        
+        # Try to query using unique fields in kwargs, in order
+        instance = None
+        for unique_field in unique_columns:
+            if unique_field in kwargs:
+                query_kwargs = {unique_field: kwargs[unique_field]}
+                instance = session.query(model).filter_by(**query_kwargs).first()
                 if instance:
-                    return instance
-                else:
-                    instance = model(**kwargs)
-                    session.add(instance)
-                    return instance
+                    break  # If a match is found, break out of the loop
+        
+        if instance:
+            # If an instance is found, update its fields (if necessary)
+            has_changes = False
+            for field, value in kwargs.items():
+                if getattr(instance, field) != value:
+                    setattr(instance, field, value)
+                    has_changes = True
+            if has_changes:
+                session.add(instance)  # Update the instance if fields have changed
+            return instance
         else:
-            instance = session.query(model).filter_by(**kwargs).first()
-            if instance:
-                return instance
-            else:
+            # If no instance is found, create a new one
+            try:
                 instance = model(**kwargs)
                 session.add(instance)
                 return instance
-
-    def get(self, model, **kwargs):
-        """Generic method to get a model instance."""
-        with self.session_scope() as session:
-            instance = session.query(model).filter_by(**kwargs).first()
-            return instance
-
-    def create(self, model, **kwargs):
-        """Generic method to create a model instance."""
-        with self.session_scope() as session:
-            instance = model(**kwargs)
-            session.add(instance)
-            return instance
+            except IntegrityError:
+                session.rollback()
+                # If an IntegrityError occurs, a duplicate unique key might have been inserted
+                # Retry fetching the instance based on unique fields
+                for unique_field in unique_columns:
+                    if unique_field in kwargs:
+                        query_kwargs = {unique_field: kwargs[unique_field]}
+                        instance = session.query(model).filter_by(**query_kwargs).first()
+                        if instance:
+                            return instance
