bd875d1c28abbd6d7a3d578450143d7507aab4aa
L3G3ND <carl.max@hotmail.de>
2024-11-09 15:38:30 +0100

    updated activity and wordle helpers

--- utils/dbhelpers/guild_config_db_helpers.py
+++ utils/dbhelpers/guild_config_db_helpers.py
@@ -16,11 +16,11 @@ GUILDS_WITH_CONFIGS = set()
 
 
 def handle_guild_config_update(dcguild: discord.Guild,
-                               error_id: int | None = None,
-                               welcome_id: int | None = None,
-                               boost_id: int | None = None,
-                               log_id: int | None = None,
-                               activity: bool | None = None) -> None:
+                                error_id: int | None = None,
+                                welcome_id: int | None = None,
+                                boost_id: int | None = None,
+                                log_id: int | None = None,
+                                activity: bool | None = None) -> None:
     with db_service.session_scope() as session:
         guild_config = get_or_create_for_guild_config(dcguild, session=session)
 
@@ -57,8 +57,8 @@ def get_all_guilds_with_configs() -> set[int]:
 
 def get_or_create_for_guild_config(dcguild: discord.Guild, session: Session) -> GuildConfig:
     """creates all relevant models for the activity updates"""
-    guild = db_service.get_or_create(Guild, guild_dc_id=dcguild.id, name=dcguild.name, session=session)
+    guild = db_service.get_or_create(Guild, session=session, guild_dc_id=dcguild.id, name=dcguild.name)
     session.flush()
-    guild_config = db_service.get_or_create(GuildConfig, session=session)
+    guild_config = db_service.get_or_create(GuildConfig, session=session, guild_id = guild.id)
     session.commit()
     return guild_config
