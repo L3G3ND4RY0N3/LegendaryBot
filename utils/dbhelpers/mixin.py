bfb6e4362a8c9f33a4b22bf7fa104e93b367923d
L3G3ND <carl.max@hotmail.de>
2024-11-10 18:14:49 +0100

    added typing

--- utils/dbhelpers/mixin.py
+++ utils/dbhelpers/mixin.py
@@ -1,5 +1,6 @@
+import typing as T
 from sqlalchemy.inspection import inspect
 
 class SerializerMixin:
-    def to_dict(self):
+    def to_dict(self) -> T.Any:
         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
