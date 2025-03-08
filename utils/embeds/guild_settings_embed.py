c11c9d519e2615d86760039098283ba682b76b77
L3G3ND <carl.max@hotmail.de>
2025-03-08 16:04:16 +0100

    config embed now works with db modulee instead of json

--- utils/embeds/guild_settings_embed.py
+++ utils/embeds/guild_settings_embed.py
@@ -1,8 +1,7 @@
 import discord
 import utils.settings as settings
-import utils.filepaths as fp
-import json
 from constants import enums as en
+from utils.dbhelpers.guild_config_db_helpers import get_config_channel_id, get_guild_config
 
 
 logger=settings.logging.getLogger("discord")
@@ -11,56 +10,50 @@ logger=settings.logging.getLogger("discord")
 #region SETUP EMBEDS
 ######## embed for the /settings command
 def createSettingEmbed(guild: discord.Guild , pageNum=0, inline=False) -> discord.Embed:
-    with open(fp.GUILD_LOG_JSON, "r") as f:
-        data = json.load(f)
-    guild_id = str(guild.id)
-    try:
-        guild_data = data[guild_id]
-    except KeyError as e:
-        logger.error(f"Keyerror for key {guild_id} in guild logging setup embedbuilder.")
-        logger.exception(f"{e}")
-    pageNum = pageNum % len(list(guild_data))
-    pageTitle: str = list(guild_data.keys())[pageNum]
+    config = get_guild_config(guild.id)
+    maxPage = len(en.GuildChannelTypes)
+    pageNum = pageNum % maxPage
+    pageTitle: str = list(en.GuildChannelTypes)[pageNum].value
     embed=discord.Embed(color=discord.Color.blurple(), title=pageTitle.title())
-    status = get_channel_status(pageTitle, guild_data)
+    channel_id = get_config_channel_id(config, pageTitle)
 
     # if not looking at activity page, where no channel is set, get the channel to display in embed, else set channel to 'None'
     if pageTitle != en.GuildChannelTypes.ACTIVITY.value: 
-        channel = guild.get_channel(guild_data.get(pageTitle, 0))
+        channel = guild.get_channel(channel_id)
 
-    match pageTitle: #will be prone to breaking if I fuck with the json...
+    match pageTitle: # will be prone to breaking if I fuck with the columns of the db model...
         case en.GuildChannelTypes.ERROR.value:
-            embed.add_field(name=f"{pageTitle.title()} channel: {status}", value="", inline=inline)
+            embed.add_field(name=f"{pageTitle.title()} channel status: {en.GuildChannelStatus.Active.name if channel_id else en.GuildChannelStatus.Inactive.name}", value="", inline=inline)
             if channel is not None:
                 embed.add_field(name="Current channel:", value=f"{channel.mention}")
             embed.add_field(name="Info", value="Set a channel where the bot will show error messages to you. The default is none.", inline=inline)
-            embed.set_footer(text=f"Page {pageNum+1} of {len(list(guild_data))}")
+            embed.set_footer(text=f"Page {pageNum+1} of {maxPage}")
 
         case en.GuildChannelTypes.LOG.value:
-            embed.add_field(name=f"{pageTitle.title()} channel: {status}", value="", inline=inline)
+            embed.add_field(name=f"{pageTitle.title()} channel status: {en.GuildChannelStatus.Active.name if channel_id else en.GuildChannelStatus.Inactive.name}", value="", inline=inline)
             if channel is not None:
                 embed.add_field(name="Current channel:", value=f"{channel.mention}")
             embed.add_field(name="Info", value="Set a channel where the bot will log events from the guild like edited/deleted messages. The default is none.", inline=inline)
-            embed.set_footer(text=f"Page {pageNum+1} of {len(list(guild_data))}")
+            embed.set_footer(text=f"Page {pageNum+1} of {maxPage}")
 
         case en.GuildChannelTypes.WELCOME.value:
-            embed.add_field(name=f"{pageTitle.title()} channel: {status}", value="", inline=inline)
+            embed.add_field(name=f"{pageTitle.title()} channel status: {en.GuildChannelStatus.Active.name if channel_id else en.GuildChannelStatus.Inactive.name}", value="", inline=inline)
             if channel is not None:
                 embed.add_field(name="Current channel:", value=f"{channel.mention}")
             embed.add_field(name="Info", value="Set a channel where the bot will show welcome messages to new joining members. The default is none.", inline=inline)
-            embed.set_footer(text=f"Page {pageNum+1} of {len(list(guild_data))}")
+            embed.set_footer(text=f"Page {pageNum+1} of {maxPage}")
 
         case en.GuildChannelTypes.BOOST.value:
-            embed.add_field(name=f"{pageTitle.title()} channel: {status}", value="", inline=inline)
+            embed.add_field(name=f"{pageTitle.title()} channel status: {en.GuildChannelStatus.Active.name if channel_id else en.GuildChannelStatus.Inactive.name}", value="", inline=inline)
             if channel is not None:
                 embed.add_field(name="Current channel:", value=f"{channel.mention}")
             embed.add_field(name="Info", value="Set a channel where the bot will send thank you messages to server booster. The default is none.", inline=inline)
-            embed.set_footer(text=f"Page {pageNum+1} of {len(list(guild_data))}")
+            embed.set_footer(text=f"Page {pageNum+1} of {maxPage}")
 
         case en.GuildChannelTypes.ACTIVITY.value:
-            embed.add_field(name=f"{pageTitle.title()} tracker: {status}", value="", inline=inline)
+            embed.add_field(name=f"{pageTitle.title()} tracker status: {en.GuildChannelStatus.Active.name if channel_id else en.GuildChannelStatus.Inactive.name}", value="", inline=inline)
             embed.add_field(name="Info", value="Activate the activity tracker, tracking the times of members in voice and written messages. Deactivated by default.", inline=inline)
-            embed.set_footer(text=f"Page {pageNum+1} of {len(list(guild_data))}")
+            embed.set_footer(text=f"Page {pageNum+1} of {maxPage}")
 
     return embed
 
